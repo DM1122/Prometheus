aec89c78543c0e49e2c409afb6316f0a2bb59ac6
DM1122 <d.maranto1122@gmail.com>
2018-09-23 11:25:55 -0400

    Removed redundant reshaper(), unshaper(), & unprocess() functions from processlib.py

--- libs/processlib.py
+++ libs/processlib.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
 import sklearn
-#from sklearn.model_selection import train_test_split as TrainTestSplit
 from sklearn.preprocessing import MinMaxScaler
 
 
-#region Functions
 def labeller(data, label, shift, dropzeros):
 
     # Drop rows w/ zero in label col
@@ -63,43 +61,6 @@ def normalizer(X_train, X_valid, X_test):
     return X_train, X_valid, X_test
 
 
-def reshaper(data_raw, timesteps):      # WIP
-    crop = data_raw.shape[0]-data_raw.shape[0]%timesteps        # modulus
-    data_raw = data_raw[:crop]      # crop to length divisble by timesteps
-
-    # data = []
-    # for i in range(data_raw.shape[0] // timesteps):     # int divisor // or subtraction -
-    #     data.append(data_raw[i*timesteps:i*timesteps+timesteps])
-    # data = np.array(data)
-    # data = np.reshape(data, (data.shape[0], data.shape[1], data_raw.shape[1]))      # samples, timesteps, features (unneccesary?)
-
-    data = []
-    for i in range(data_raw.shape[0] - timesteps):     # int divisor // or subtraction -
-        data.append(data_raw[i:i+timesteps])
-    data = np.array(data)
-    
-    return data
-
-
-def unshaper(data_raw):     # WIP
-    data_raw = np.reshape(data_raw, (data_raw.shape[0], data_raw.shape[1]))       # samples, features
-
-    data = []
-    data.append(data_raw[0:1])      # append 
-    data = np.array(data)
-    data = np.reshape(data, (data.shape[2],1))
-
-    for i in range(data_raw.shape[0] - 1):
-        block = []
-        block.append(data_raw[i+1:i+2, data_raw.shape[1]-1:])
-        block = np.array(block)
-        block = np.reshape(block, (block.shape[2],1))
-
-        data = np.concatenate((data, block), axis=0)
-
-    return data
-
-
 def process(data, label, shift, dropzeros, split_valid, split_test, model, timesteps):
     '''
     Wrapper function for all processing steps.
@@ -113,35 +74,5 @@ def process(data, label, shift, dropzeros, split_valid, split_test, model, times
 
     # Normalize
     X_train, X_valid, X_test = normalizer(X_train, X_valid, X_test)
-
-    # # Reshape (RNNs only)
-    # if model == 'RNN' or model == 'LSTM' or model == 'GRU':
-    #     X_train = reshaper(X_train, timesteps)
-    #     y_train = reshaper(y_train, timesteps)
-    #     X_test = reshaper(X_test, timesteps)
-    #     y_test = reshaper(y_test, timesteps)
     
     return X_train, y_train, X_valid, y_valid, X_test, y_test
-
-
-def unprocess(output_raw, y_raw, model):        # WIP
-    '''
-    Wrapper function for all unprocessing steps.
-    '''
-
-    # Unshape RNNs
-    if model == 'RNN' or model == 'LSTM' or model == 'GRU':
-        output_raw = unshaper(output_raw)
-        y_raw = unshaper(y_raw)
-    
-    # Convert to df
-    output = pd.DataFrame(output_raw, columns=['Output'])
-    y = pd.DataFrame(y_raw, columns=['Label'])
-
-    # Concat dfs
-    data_comp = pd.concat([output, y], axis=1, sort=False)
-    data_comp.index.names = ['Index']
-
-    return data_comp
-
-#endregion
