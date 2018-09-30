93ccff88986f645079aa87137f5cc26676a36cf7
DM1122 <d.maranto1122@gmail.com>
2018-09-30 11:03:46 -0400

    Updated epimetheus.py

--- epimetheus.py
+++ epimetheus.py
@@ -3,38 +3,41 @@ import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 import os
-import prometheus
-import shutil
 import skopt
 import tensorflow as tf
 from tensorflow import keras
+
+import libs
+from libs import figurelib, modelib, NSRDBlib, processlib
+import prometheus
 
 np.random.seed(123)
 tf.set_random_seed(123)
 
 
 #region Metaparams
-params_search_calls = 11       # must be >=11 (risk 'The objective has been evaluated at this point before' w/ values >100)
+params_search_calls = 11
 
 learn_rate_space = skopt.space.Real(low=1e-6, high=1e-2, prior='log-uniform', name='learn_rate')
 layers_space = skopt.space.Integer(low=1, high=3, name='n_layers')
-nodes_space = skopt.space.Integer(low=2, high=512, name='n_nodes')
+nodes_space = skopt.space.Integer(low=2, high=256, name='n_nodes')
 act_space = skopt.space.Categorical(categories=['relu'], name='act')
-dropout_space = skopt.space.Real(low=0, high=0.5, name='dropout')
+droprate_space = skopt.space.Real(low=0, high=0.5, name='droprate')
+batch_space = skopt.space.Integer(low=1, high=256, name='batch_size')
 
-params = [learn_rate_space, layers_space, nodes_space, act_space, dropout_space]
-params_init = [3e-5, 1, 16, 'relu', 0.2]
+params = [learn_rate_space, layers_space, nodes_space, act_space, droprate_space, batch_space]
+params_init = [3e-5, 1, 8, 'relu', 0.0, 128]
 #endregion
 
 file_name = os.path.basename(__file__)
 
-#region Functions
-@skopt.utils.use_named_args(dimensions=params)      # allows params to be passed as list
-def fitness(learn_rate, n_layers, n_nodes, act, dropout):
 
-    model, fitness, log_dir = prometheus.train_model(learn_rate, n_layers, n_nodes, act, dropout)
+@skopt.utils.use_named_args(dimensions=params)
+def fitness(learn_rate, n_layers, n_nodes, act, droprate, batch_size):
 
-    update_best_model(model, fitness, log_dir)
+    model, fitness, log = prometheus.train_model(learn_rate, n_layers, n_nodes, act, droprate, batch_size)
+
+    modelib.update_best_model(model=model, loss=fitness, log=log, logdir='./logs/prometheus.py/', id=file_name)
 
     del model
     keras.backend.clear_session()
@@ -42,51 +45,12 @@ def fitness(learn_rate, n_layers, n_nodes, act, dropout):
     return fitness
 
 
-def update_best_model(model, loss, log_dir):
-    global loss_best
-    try:        # will set best loss to current loss on first run
-        loss_best
-    except NameError:
-        loss_best = loss
-
-    # determine best model
-    if loss <= loss_best:
-        if not os.path.exists('./models/'+file_name):      # model.save() does not explicitly create dir
-            os.makedirs('./models/'+file_name)
-
-        print('New best!')
-        loss_best = loss      # update best accuracy
-        model.save('./models/'+file_name+'/model.keras')     # save best model to disk
-
-        # delete all other model logs
-        for log in os.listdir('./logs/'):
-            if ('./logs/'+log+'/') != log_dir:
-                shutil.rmtree('./logs/'+log+'/')
-    else:
-        shutil.rmtree(log_dir)      # delete current log
-
-
-def plot_results(search):
-    matplotlib.style.use('classic')
-    fig1 = skopt.plots.plot_convergence(search)
-    fig2, ax = skopt.plots.plot_objective(result=search, dimension_names=['learn_rate','n_layers','n_nodes','dropout'])
-    fig3, ax = skopt.plots.plot_evaluations(result=search, dimension_names=['learn_rate','n_layers','n_nodes','dropout'])
-
-    # Save plot to disk
-    plot_date = dt.datetime.now().strftime('%Y%m%d-%H%M%S')
-    plot_dir = './plots/'+file_name+'/'+plot_date+'/'
-    os.makedirs(plot_dir)
-    # fig1.savefig(plot_dir+'convergence_plot.png')      # does not work, must save manually!
-    fig2.savefig(plot_dir+'objective_plot.png')
-    fig3.savefig(plot_dir+'evaluations_plot.png')
-
-    plt.show()
-#endregion
-
-
 if __name__ == '__main__':
     print('Commencing Epimetheus hyperparameter optimization...')
+
+    #region Hyperparameter optimization
     time_start = dt.datetime.now()
+
     params_search = skopt.gp_minimize(      # bayesian optimization
         func=fitness,
         dimensions=params,
@@ -94,6 +58,7 @@ if __name__ == '__main__':
         n_calls=params_search_calls,
         x0=params_init
     )
+
     time_end = dt.datetime.now()
     time_elapsed = time_end - time_start
 
@@ -102,10 +67,17 @@ if __name__ == '__main__':
     print('Hyperparameter optimization completed!')
     print('Results: ', params_search.space.point_to_dict(params_search.x))
     print('Fitness: ', params_search.fun)
-    print('Elapsed time: {}'.format(time_elapsed))
-    plot_results(params_search)
+    print('Elapsed time: ', time_elapsed)
+    #endregion
 
+    #region Plots
+    fig1, fig2, fig3 = figurelib.plot_opt(search=params_search, dims=['learn_rate', 'n_layers', 'n_nodes', 'droprate', 'batch_size'])
 
-    print('Debug:\n$tensorboard --logdir=logs')
+    plot_date = dt.datetime.now().strftime('%Y%m%d-%H%M%S')
+    figurelib.save_fig(fig=fig1, name='convergence_plot.png', date=plot_date, id=file_name)
+    figurelib.save_fig(fig=fig2, name='objective_plot.png', date=plot_date, id=file_name)
+    figurelib.save_fig(fig=fig3, name='evaluations_plot.png', date=plot_date, id=file_name)
 
+    plt.show()
+    #endregion
 
