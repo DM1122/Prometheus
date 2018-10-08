42aabd8cc8c9479159d048aa9ac0a30e3489108e
DM1122 <d.maranto1122@gmail.com>
2018-10-07 23:20:02 -0400

    Removed test_model() func from prometheus_rnn.py

--- prometheus_rnn.py
+++ prometheus_rnn.py
@@ -14,7 +14,7 @@ tf.set_random_seed(123)
 
 #region Hyperparams
 model_type = 'RNN'      # RNN/LSTM/GRU
-n_epochs = 100
+n_epochs = 1
 n_epoch_steps = 8
 learn_rate = 0.001
 n_layers = 1
@@ -175,37 +175,9 @@ def train_model(learn_rate=learn_rate, n_layers=n_layers, n_nodes=n_nodes, act=a
     return model, loss, log
 
 
-def test_model(model):
-    print('Testing model...')
-
-    #region Testing
-    y_pred = model.predict(x=np.expand_dims(X_test, axis=0), batch_size=None, verbose=0, steps=None)        # predict using test data
-    y_pred = y_pred.reshape(y_pred.shape[1],1)
- 
-    global y_test       # unscale datasets
-    y_test = y_scl.inverse_transform(y_test)
-    y_pred = y_scl.inverse_transform(y_pred)
-
-    if (features_log):
-        y_test = np.exp(y_test)
-        y_pred = np.exp(y_pred)
-    #endregion
-
-    #region Plots
-    fig = figurelib.plot_pred_warmup(lbl=y_test, out=y_pred, warmup=warmup_length, xlbl='Index', ylbl='DHI [W/$m^2$]')
-
-    plot_date = dt.datetime.now().strftime('%Y%m%d-%H%M%S')
-    figurelib.save_fig(fig=fig, name='output_plot.png', date=plot_date, id=file_name)
-
-    plt.show()
-    #endregion
-
-
 if __name__ == '__main__':
     print('Commencing Prometheus model generation...')
 
-    model, _, _ = train_model()
-
-    test_model(model)
+    train_model()
 
     print('Debug:\n$tensorboard --logdir=logs/'+file_name)
